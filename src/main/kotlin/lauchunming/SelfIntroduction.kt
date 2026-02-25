bcbe845b9227cf9623082dda67843f71a8c20f75
Lau Chun Ming <23238240@life.hkbu.edu.hk>
2026-02-25 17:06:24 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/lauchunming/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.lauchunming
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Lau Chun Ming.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
